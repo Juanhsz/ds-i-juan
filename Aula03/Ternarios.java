41a8e79592f44755a6934605e6bf6b5982d1cc19
juanhsz <juan.hernandes@etec.sp.gov.br>
2024-04-15 09:03:44 -0300

    Aula sobre condições e laços

---
+++ Aula03/Ternarios.java
@@ -0,0 +1,9 @@
+public class ternarios{
+    public static void main(String[] args){
+        int time = 20;
+        String result;
+
+        result = (time < 18) ? "Good Day" : "Good Evening";
+        System.out.println(result);
+    }
+}
